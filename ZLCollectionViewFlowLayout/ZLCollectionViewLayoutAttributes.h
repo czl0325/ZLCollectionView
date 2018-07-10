2a821bac9c78225855ec8edd77485c6503a929fe
陈昭良 <295183917@qq.com>
2018-07-10 10:26:37 +0800

    提交0.5版本

---
+++ ZLCollectionViewFlowLayout/ZLCollectionViewLayoutAttributes.h
@@ -0,0 +1,15 @@
+//
+//  ZLCollectionViewLayoutAttributes.h
+//  ZLCollectionView
+//
+//  Created by zhaoliang chen on 2018/7/9.
+//  Copyright © 2018年 zhaoliang chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZLCollectionViewLayoutAttributes : UICollectionViewLayoutAttributes
+
+@property(nonatomic,strong)UIColor* color;
+
+@end
